7c0e01c723d08b959f7eb4d478174401f5f4dcac
Dominik Dudás <dudasdominik3@gmail.com>
2023-01-09 12:05:06 +0100

    outdated

--- ngo/src/App.js
+++ ngo/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import logo from './monke.png';
 import './App.css';
+
+function divComponent (className){
+  return (
+    <div className={className}>
+      
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
 
