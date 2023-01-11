a8cc6e0310af5b9fa1a4714df364461ffd07a038
Dominik Dudás <dudasdominik3@gmail.com>
2023-01-11 14:29:22 +0100

    Console logging the data

--- ngo/src/components/ContactCard.js
+++ ngo/src/components/ContactCard.js
@@ -1,19 +1,30 @@
 import React, { useState } from 'react'
 import Donation from './Donation';
+function gatherLogData() {
+	const data = {
+		name: document.getElementById('fname').value,
+		email: document.getElementById('femail').value,
+		city: document.getElementById('fcity').value,
+		adress: document.getElementById('faddress').value,
+}
+console.log(data)
+
+}
+
 function ContactCard () {
 	const [isHidden, setIsHidden] = useState(false);
-
+	console.log(document.getElementById('form'))
     return (
 		<div className='formContainer'>
         <div className='actualForm' style={{display: isHidden ? 'none' : 'flex'}}>
-    <form onSubmit={(e) => {e.preventDefault() ; setIsHidden(!isHidden)}} id="form">
-	<label class='label' for="fname">Name:</label><br></br>
+    <form onSubmit={(e) => {e.preventDefault() ; gatherLogData(); setIsHidden(!isHidden)}} id="form">
+	<label class='label' for="fname" name="name">Name:</label><br></br>
 	<input type="text" id="fname" class="form" required></input><br></br>
-	<label class='label'for="lname">Email:</label><br></br>
+	<label class='label'for="lname" name="email">Email:</label><br></br>
 	<input type="email" id="femail" class="form" required></input><br></br>
-	<label class='label' for="lname">City:</label><br></br>
+	<label class='label' for="lname" name="city">City:</label><br></br>
 	<input type="text" id="fcity" class="form" required></input><br></br>
-	<label class='label' for="lname">Address:</label><br></br>
+	<label class='label' for="lname" name="address">Address:</label><br></br>
 	<input type="text" id="faddress" class="form" required></input><br></br>
     <input type="submit" value="Submit"></input>
   </form>
@@ -23,7 +34,6 @@ function ContactCard () {
 		</div>
 		<Donation/>
 		</div>
-		
     )
 }
 
